e92557ca5e4bbb886d366f4f8e1fbfc49c0df32d
Lucien Morey <lucienp@live.com.au>
2020-01-18 02:20:16 +1100

    created shooter component and switched to a magicbot framework

--- robot.py
+++ robot.py
@@ -6,18 +6,50 @@
 # the project.
 
 import wpilib
-import wheel_of_fortune.WheelOfFortune
+import rev
 
-class MyRobot(wpilib.TimedRobot):
-	"""
-		This is a simple example to show the values that can be read from the REV
-		Color Sensor V3
-	"""
+import magicbot
 
-	def robotInit(self):
-		# Initialise components
-		wheel = WheelOfFortune()
+from components.shooter import Shooter
+
+class MyRobot(magicbot.MagicRobot):
+    shooter: Shooter
+
+    
+    def createObjects(self):
+        """Robot initialization function"""
+
+        # object that handles basic drive operations
+        self.joystick = wpilib.Joystick(0)
+
+        self.shooter_outer_motor = rev.CANSparkMax(3, rev.MotorType.kBrushless)
+        self.shooter_centre_motor = rev.CANSparkMax(2, rev.MotorType.kBrushless) 
+
+        
+
+        self.loading_piston = wpilib.DoubleSolenoid(0, 1)
+
+        
+        
+
+    def teleopInit(self):
+        """Executed at the start of teleop mode"""
+        
+
+
+    def teleopPeriodic(self):
+        """Runs the motors with tank steering"""
+
+        wpilib.SmartDashboard.putNumber("outerVelocity", self.shooter.get_outer_error())
+        wpilib.SmartDashboard.putNumber("centreVelocity", self.shooter.get_centre_error())
+
+        if self.joystick.getRawButtonPressed(11):
+            self.loading_piston.set(wpilib.DoubleSolenoid.Value.kForward)
+        if self.joystick.getRawButtonPressed(12):
+            self.loading_piston.set(wpilib.DoubleSolenoid.Value.kReverse)
+
+
 
 
 if __name__ == "__main__":
-	wpilib.run(MyRobot)
+    wpilib.run(MyRobot)
