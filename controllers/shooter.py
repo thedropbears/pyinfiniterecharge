a7a86363a66ad38870bd45a615196764608f3c59
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2020-01-25 14:24:00 +1100

    shooter statemachine skeleton

--- controllers/shooter.py
+++ controllers/shooter.py
@@ -1,13 +1,33 @@
+from magicbot import StateMachine, state
+
+from components.turret import Turret
 from components.indexer import Indexer
+from components.shooter import Shooter
 
-class ShooterController:
-    """Class to control the entire shooter conglomerate"""
+class ShooterController(StateMachine):
+    """Statemachine for high level control of the shooter and injector"""
 
+    turret: Turret
     indexer: Indexer
+    shooter: Shooter
 
-    def __init__(self) -> None:
+    @state
+    def searching(self):
+        """
+        The vision system does not have a target, we try to find one using odometry
+        """
         pass
 
-    def execute(self) -> None:
-        """Executed every cycle"""
+    @state
+    def tracking(self):
+        """
+        Aiming towards a vision target and spining up flywheels
+        """
+        pass
+
+    @state
+    def firing(self):
+        """
+        Positioned to fire, inject and expel a single ballball
+        """
         pass
