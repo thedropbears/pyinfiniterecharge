57947d98b43fbb7becf2a41476f11cbc04c60448
Injector-Spenral <sam.richards@thedropbears.org.au>
2020-01-25 11:52:37 +1100

    Changed from NetworkTablesInstance to NetworkTables

--- components/vision.py
+++ components/vision.py
@@ -1,16 +1,15 @@
-from networktables import NetworkTablesInstance
+from networktables import NetworkTables
 
 
 class Vision:
     def __init__(self) -> None:
-        self.ntinst = NetworkTablesInstance()
-        self.ntinst.startClient("10.47.74.6")
-        self.ntinst.setUpdateRate(1)
-
-        self.vision_data_entry = self.ntinst.getEntry("/vision/vision_data")
+        self.nt = NetworkTables
+        self.table = self.nt.getTable("/vision")
+        self.entry = self.table.getEntry("data")
+        self.nt.setUpdateRate(1)
 
     def get_vision_data(self) -> tuple:
         """Returns a tuple containing the distance (metres), angle (radians), and timestamp (time.monotonic)
         If it can't get info, it returns [-1.0, -1.0, -1.0]
         """
-        self.vision_data_entry.getDoubleArray([-1.0, -1.0, -1.0])
+        self.entry.getDoubleArray([-1.0, -1.0, -1.0])
