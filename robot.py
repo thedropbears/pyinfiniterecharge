d40af56eb6a69586eebb837e31ec5d4dfe0b6410
Oliver Warrick <oliver.warrick@thedropbears.org.au>
2020-01-27 12:12:39 +1100

    moved joystick handling

--- robot.py
+++ robot.py
@@ -28,9 +28,6 @@ class MyRobot(magicbot.MagicRobot):
     def createObjects(self):
         """Robot initialization function"""
         # object that handles basic drive operations
-        self.joystick_left = wpilib.Joystick(0)
-        self.joystick_right = wpilib.Joystick(1)
-        self.spinner_joystick = wpilib.Joystick(2)
 
         self.shooter_outer_motor = rev.CANSparkMax(3, rev.MotorType.kBrushless)
         self.shooter_centre_motor = rev.CANSparkMax(2, rev.MotorType.kBrushless)
@@ -44,48 +41,52 @@ class MyRobot(magicbot.MagicRobot):
         self.spinner_solenoid = wpilib.Solenoid(2)
         self.colour_sensor = rev.color.ColorSensorV3(wpilib.I2C.Port.kOnboard)
 
+        self.joysticks = joystick_handlers()
+
     def teleopInit(self):
         """Executed at the start of teleop mode"""
 
     def teleopPeriodic(self):
-
         """Executed every cycle"""
+
+        self.joysticks.handle_indexer_inputs(joystick = self.joysticks.joystick_left, index_controller = self.indexer)
+        self.joysticks.handle_spinner_inputs(joystick = self.joysticks.spinner_joystick, spinner_controller = self.spinner)
+        self.joysticks.handle_shooter_inputs(joystick_left = self.joysticks.joystick_left, joystick_right = self.joysticks.joystick_right)
+
+
+class joystick_handlers:
+    def __init__(self):
+        self.joystick_left = wpilib.Joystick(0)
+        self.joystick_right = wpilib.Joystick(1)
+        self.spinner_joystick = wpilib.Joystick(2)
+
+    def handle_indexer_inputs(self, joystick = None, shooter_controller = None, indexer_controller = None):
+        if joystick.getTrigger():
+            shooter_controller.eject_cells()
+
+        if joystick.getRawButtonPressed(3):
+            shooter_controller.shoot_cells()
+
+        if joystick.getRawButtonPressed(4):
+            shooter_controller.intake_cells()
+
+    def handle_spinner_inputs(self, joystick = None, spinner_controller = None):
+        if joystick.getRawButtonPressed(7):
+            spinner_controller.run(test=True, task="position")
+            print(f"Position Control")
+
+        if joystick.getRawButtonPressed(8):
+            spinner_controller.run(test=True, task="rotation")
+            print(f"Rotation Control")
+    
+    def handle_shooter_inputs(self, joystick_left = None, joystick_right = None, loading_piston = None, shooter = None):
         outer_throttle = ((-self.joystick_left.getThrottle() + 1) / 2) * 5000
         inner_throttle = -((-self.joystick_right.getThrottle() + 1) / 2) * 5000
+        if loading_piston != None:
+            shooter.set_motor_rpm(outer_throttle, inner_throttle)
 
-        self.shooter.set_motor_rpm(outer_throttle, inner_throttle)
-
-        wpilib.SmartDashboard.putNumber("outerError", self.shooter.get_outer_error())
-        wpilib.SmartDashboard.putNumber("centreError", self.shooter.get_centre_error())
-
-        wpilib.SmartDashboard.putNumber("outerVelocity", outer_throttle)
-        wpilib.SmartDashboard.putNumber("centreVelocity", inner_throttle)
-
-        if self.joystick_left.getRawButtonPressed(11):
-            self.loading_piston.startPulse()
-
-        # wpilib.SmartDashboard.putString("Colour wheel state", self.spinner_controller.state)
-        self.handle_indexer_inputs(self.joystick_left)
-        self.handle_spinner_inputs(self.spinner_joystick)
-
-    def handle_indexer_inputs(self, joystick):
-        if joystick.getTrigger():
-            # self.shooter_controller.next_state("eject_cells")
-            self.shooter_controller.eject_cells()
-        if joystick.getRawButtonPressed(3):
-            # self.shooter_controller.next_state("shoot_cells")
-            self.shooter_controller.shoot_cells()
-        if joystick.getRawButtonPressed(4):
-            # self.shooter_controller.next_state("intake_cells")
-            self.shooter_controller.intake_cells()
-
-    def handle_spinner_inputs(self, joystick):
-        if joystick.getRawButtonPressed(7):
-            self.spinner_controller.run(test=True, task="position")
-            print(f"Position Control")
-        if joystick.getRawButtonPressed(8):
-            self.spinner_controller.run(test=True, task="rotation")
-            print(f"Rotation Control")
+        if joystick_left.getRawButtonPressed(11):
+            loading_piston.startPulse()
 
 
 if __name__ == "__main__":
