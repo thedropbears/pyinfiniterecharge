63ff8a6a1347056b3d3074813be9d59c293110dc
Injector-Spenral <sam.richards@thedropbears.org.au>
2020-01-27 17:15:30 +1100

    Changed scale_value tuples to seperate floats

--- utilities/scale_value.py
+++ utilities/scale_value.py
@@ -1,10 +1,9 @@
-from typing import Tuple
-
-
 def scale_value(
     value: float,
-    input_range: Tuple[float, float],
-    output_range: Tuple[float, float],
+    input_lower: float,
+    input_upper: float,
+    output_lower: float,
+    output_upper: float,
     exponent: float = 1,
 ) -> float:
     """Scales a value based on the input range and output range.
@@ -12,8 +11,8 @@ def scale_value(
         self.scale_value(joystick.getThrottle(), (1, -1), (0, 1))
     The output is then raised to the exponent argument. Be careful of complex numbers and raising zero to negative powers.
     """
-    input_distance = input_range[1] - input_range[0]
-    output_distance = output_range[1] - output_range[0]
-    ratio = (value - input_range[0]) / input_distance
-    result = ratio * output_distance + output_range[0]
+    input_distance = input_upper - input_lower
+    output_distance = output_upper - output_lower
+    ratio = (value - input_lower) / input_distance
+    result = ratio * output_distance + output_lower
     return result ** (exponent - 1) * abs(result)  # Fancy exponent to preserve sign.
