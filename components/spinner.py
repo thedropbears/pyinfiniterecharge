c28f52a35c5d0030201423a0662f4eb8db2e6246
Oliver Warrick <oliver.warrick@thedropbears.org.au>
2020-02-03 20:53:07 +1100

    added comments, changed piston_up to raise_wheel

--- components/spinner.py
+++ components/spinner.py
@@ -10,6 +10,8 @@ class Colour:
         self.blue = blue
 
     def dist(self, c) -> tuple:
+        # finds the "distance" of one colour from another
+        # the "distance" is the sum of the differences in rgb value
         return (
             abs(self.red - c.red) + abs(self.green - c.green) + abs(self.blue - c.blue)
         )
@@ -32,9 +34,10 @@ class Spinner:
 
     WHEEL_ORDER = ["R", "Y", "B", "G"]
 
-    POSITION_SPIN_FACTOR = 0.3
+    POSITION_SPIN_FACTOR = 0.3  # position control will run at up to twice this speed
 
-    ROTATION_SPIN_FACTOR = 20
+    # rotation control uses the formula: 1 - ROTATION_SPIN_FACTOR ** (rotations - ROTATION_TARGET_ROTATIONS)  for wheel speed
+    ROTATION_SPIN_FACTOR = 20  # increase this to recude slow down
     ROTATION_MAX_SPEED = 1
     ROTATION_TARGET_ROTATIONS = 3.5
 
@@ -43,7 +46,7 @@ class Spinner:
 
     def setup(self):
         self.spinner_motor.set(0)
-        self.piston_up()
+        self.raise_wheel()
         self.rotations = 0
         # self.state = None
         self.piston_state = "down"
@@ -52,17 +55,17 @@ class Spinner:
 
     def on_enable(self) -> None:
         self.spinner_motor.set(0)
-        self.piston_up()
+        self.raise_wheel()
 
-    def piston_up(self) -> None:
+    def raise_wheel(self) -> None:  # moves wheel up
         self.spinner_solenoid.set(False)
         self.piston_state = "up"
 
-    def piston_down(self) -> None:
+    def lower_wheel(self) -> None:  # moves wheel down
         self.spinner_solenoid.set(True)
         self.piston_state = "down"
 
-    def get_current_colour(self) -> str:
+    def get_current_colour(self) -> str:  # reads and catagorizes colour
         self.sensed_colour = self.colour_sensor.getColor()
 
         distances = {}
@@ -76,6 +79,8 @@ class Spinner:
             return min(distances, key=lambda col: distances[col])
 
     def get_wheel_dist(self) -> int:
+        # find the distance of the colour wheel from desired state in segments
+        # reuturns between -2 and 2 with 0 being the colour desired
         current_colour = (
             self.get_current_colour()
         )  # only called when running position control
@@ -90,29 +95,30 @@ class Spinner:
             return distance - 4
         return distance
 
-    def go_to_colour(self, colour: str):
+    def go_to_colour(self, colour: str) -> bool:  # do position control
         self.required_colour = colour
         distance = self.get_wheel_dist()
         self.spinner_motor.set(distance * self.POSITION_SPIN_FACTOR)
-        print("position control ran")
+
         if distance == 0:
             return True
         else:
             return False
 
-    def do_rotation(self) -> None:
+    def do_rotation(self) -> bool:
         spin_speed = 1 - (
             self.ROTATION_SPIN_FACTOR
             ** (self.rotations - self.ROTATION_TARGET_ROTATIONS)
             * self.ROTATION_MAX_SPEED
         )
-        self.spinner_motor.set(spin_speed)
-        print("rotation control ran")
         # speed slows down as it gets closer to required revolutions
+        self.spinner_motor.set(spin_speed)
+
         current_colour = self.get_current_colour()
-        if self.lastCol != current_colour:
+        if self.lastCol != current_colour:  # if it has moved along in segments
             self.rotations += 1 / 8
         self.lastCol = current_colour
+
         if self.rotations >= 3.5:
             self.rotations = 0
             self.spinner_motor.set(0)
