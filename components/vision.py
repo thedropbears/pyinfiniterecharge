e2efd895230c66835f390cabbc98f86be6c6976a
Lucien Morey <lucienp@live.com.au>
2020-02-07 18:23:49 +1100

    renamed vision entry member

--- components/vision.py
+++ components/vision.py
@@ -6,7 +6,7 @@ class Vision:
     def __init__(self) -> None:
         self.nt = NetworkTables
         self.table = self.nt.getTable("/vision")
-        self.entry = self.table.getEntry("data")
+        self.vision_data_entry = self.table.getEntry("data")
         self.lastTime = 0
         self.repeats = 0
         self.data = [None, None, None]
@@ -18,11 +18,11 @@ class Vision:
         If the pi vision cant see the target it will not send to network tables, meaning this will continue
         to return the most recently seen target position and the time it was seen
         """
-        self.data = self.entry.getDoubleArray([None, None, None])
+        self.data = self.vision_data_entry.getDoubleArray([None, None, None])
         return self.data
 
     def is_ready(self) -> bool:
-        if self.entry.exists() and self.data[2] != None:
+        if self.vision_data_entry.exists() and self.data[2] != None:
             if self.data[2] == self.lastTime:  # if it gets the same time twice
                 self.repeats += 1
             else:
