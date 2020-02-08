ee733fefefcc20124a5c6260ea796db0d55ce7c8
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2020-02-09 10:41:28 +1100

    initial odometry

--- components/chassis.py
+++ components/chassis.py
@@ -1,4 +1,7 @@
+import math
+
 import rev
+import wpilib
 from wpilib.drive import DifferentialDrive
 from typing import Tuple
 
@@ -9,6 +12,10 @@ class Chassis:
     chassis_right_rear: rev.CANSparkMax
     chassis_right_front: rev.CANSparkMax
 
+    WHEEL_RADIUS = 0.076
+    GEAR_REDUCTION = 1/10.75
+    COUNTS_TO_METERS = GEAR_REDUCTION * WHEEL_RADIUS * math.tau
+
     def setup(self) -> None:
         self.chassis_left_rear.setInverted(False)
         self.chassis_left_front.setInverted(False)
@@ -18,12 +25,21 @@ class Chassis:
         self.chassis_left_rear.follow(self.chassis_left_front)
         self.chassis_right_rear.follow(self.chassis_right_front)
 
+        self.left_encoder = self.chassis_left_front.getEncoder()
+        self.right_encoder = self.chassis_right_front.getEncoder()
+
+        self.odometry_tracker = wpilib.kinematics.DifferentialDriveOdometry()
+
         self.diff_drive = DifferentialDrive(
             self.chassis_left_front, self.chassis_right_front
         )
 
     def execute(self) -> None:
+        heading = self.get_heading()
+        left_travel = self.left_encoder.getPosition() * self.COUNTS_TO_METERS
+        right_travel = self.right_encoder.getPosition() * self.COUNTS_TO_METERS
         self.diff_drive.arcadeDrive(self.vx, self.vz, squareInputs=False)
+        self.odometry_tracker.update(heading, left_travel, right_travel)
 
     def drive(self, vx: float, vz: float) -> None:
         """Drive the robot with forwards velocity and rotational velocity
