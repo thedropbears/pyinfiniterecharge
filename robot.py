d6838a2cace73911b10ffde4e7f8ed9a62fc1eea
David Vo <auscompgeek@users.noreply.github.com>
2020-02-09 17:29:27 +1100

    Make 3 m/s the top speed of the drivetrain

--- robot.py
+++ robot.py
@@ -118,8 +118,8 @@ class MyRobot(magicbot.MagicRobot):
 
     def handle_chassis_inputs(self, joystick):
         scaled_throttle = scale_value(joystick.getThrottle(), 1, -1, 0, 1)
-        vx = scale_value(joystick.getY(), 1, -1, -1, 1, 3) * scaled_throttle
-        vz = scale_value(joystick.getX(), 1, -1, -1, 1, 3) * scaled_throttle
+        vx = scale_value(joystick.getY(), 1, -1, -3, 3, 2) * scaled_throttle
+        vz = scale_value(joystick.getX(), 1, -1, -3, 3, 2) * scaled_throttle
         self.chassis.drive(vx, vz)
 
     def handle_shooter_inputs(self, joystick: wpilib.Joystick):
