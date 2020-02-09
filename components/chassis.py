e5376b01763f59150764f4c0e403310ce7cbb659
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2020-02-09 11:37:09 +1100

    add reset pose and encode in real world units

--- components/chassis.py
+++ components/chassis.py
@@ -30,23 +30,28 @@ class Chassis:
         self.chassis_left_rear.follow(self.chassis_left_front)
         self.chassis_right_rear.follow(self.chassis_right_front)
 
-        self.left_encoder = self.chassis_left_front.getEncoder()
-        self.right_encoder = self.chassis_right_front.getEncoder()
+        self.left_encoder: rev.CANEncoder = self.chassis_left_front.getEncoder()
+        self.right_encoder: rev.CANEncoder = self.chassis_right_front.getEncoder()
+        for encoder in (self.left_encoder, self.right_encoder):
+            # measure encoder outputs in m and m/s
+            encoder.setPositionConversionFactor(self.COUNTS_TO_METERS)
+            encoder.setVelocityConversionFactor(self.COUNTS_TO_METERS * 60)
 
         rotation = wpilib.geometry.Rotation2d(0)
 
         self.odometry_tracker = wpilib.kinematics.DifferentialDriveOdometry(rotation)
+        # if reset_pose is not called, the robot assumes it started at 0, 0
 
         self.diff_drive = DifferentialDrive(
             self.chassis_left_front, self.chassis_right_front
         )
 
     def execute(self) -> None:
-        heading = self.imu.getAngle()
-        left_travel = self.left_encoder.getPosition() * self.COUNTS_TO_METERS
-        right_travel = self.right_encoder.getPosition() * self.COUNTS_TO_METERS
+        rotation = wpilib.geometry.Rotation2d(self.imu.getAngle())
+        left_travel = self.left_encoder.getPosition()
+        right_travel = self.right_encoder.getPosition()
         self.diff_drive.arcadeDrive(self.vx, self.vz, squareInputs=False)
-        self.odometry_tracker.update(heading, left_travel, right_travel)
+        self.odometry_tracker.update(rotation, left_travel, right_travel)
 
     def drive(self, vx: float, vz: float) -> None:
         """Drive the robot with forwards velocity and rotational velocity
@@ -58,3 +63,10 @@ class Chassis:
     def get_pose(self) -> wpilib.geometry.Pose2d:
         """Return our position and heading as a pose."""
         return self.odometry_tracker.getPose()
+
+    def reset_pose(self, pose: wpilib.geometry.Pose2d) -> None:
+        """To be called by autonomous to set our start position"""
+        self.left_encoder.setPosition(0)
+        self.right_encoder.setPosition(0)
+        self.imu.resetHeading()
+        self.odometry_tracker.resetPosition(pose, 0)
