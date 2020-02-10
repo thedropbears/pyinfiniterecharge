76d7c226b9e8ff6676e313b9496a208cae1aa3dd
David Vo <auscompgeek@users.noreply.github.com>
2020-02-10 17:39:50 +1100

    Print git branch along with commit
    --broken also implies --dirty it seems.

--- utilities/git.py
+++ utilities/git.py
@@ -5,13 +5,16 @@ import subprocess
 
 def describe() -> str:
     robot_dir = pathlib.Path(__file__).parents[1]
+    # would use a hidden file here, but deploy skips dotfiles
     fname = robot_dir / "gitid"
     if os.path.isdir(".git"):
-        desc = subprocess.check_output(
-            ("git", "describe", "--broken", "--always", "--tags", "--dirty"),
-            encoding="utf-8",
-        )
-        # would use a hidden file here, but deploy skips dotfiles
+        commit_hash = subprocess.check_output(
+            ("git", "describe", "--broken", "--always", "--tags"), text=True
+        ).strip()
+        ref = subprocess.check_output(
+            ("git", "describe", "--all", "--dirty=", "--broken="), text=True
+        ).strip()
+        desc = f"{commit_hash} ({ref})"
         with open(fname, "w") as f:
             f.write(desc)
         return desc
