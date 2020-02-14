878a6b7338c365becc4400f06fcdf9995a1bf228
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2020-02-15 10:11:14 +1100

    Fix initial auto

--- Autonomous/shoot_move_shoot.py
+++ Autonomous/shoot_move_shoot.py
@@ -1,7 +1,8 @@
 import math
 from typing import Optional
 
-import wpilib
+import wpilib.geometry
+import wpilib.controller
 from magicbot import AutonomousStateMachine, state
 
 from components.chassis import Chassis
@@ -19,14 +20,16 @@ def to_pose(x: float, y: float, heading: float) -> wpilib.geometry.Pose2d:
 
 class ShootMoveShootBase(AutonomousStateMachine):
 
-    shootercontroller: ShooterController
+    shooter_controller: ShooterController
 
     chassis: Chassis
     indexer: Indexer
 
     def __init__(self) -> None:
         super().__init__()
-        self.controller = wpilib.RamseteController()
+        self.controller = wpilib.controller.RamseteController()
+        tolerance = to_pose(0.1, 0.1, math.pi/18)
+        self.controller.setTolerance(tolerance)
         self.pos_tolerance = 0.1  # m
         self.ang_tolerance = math.pi / 18  # rad
         # TODO tune these
@@ -36,17 +39,21 @@ class ShootMoveShootBase(AutonomousStateMachine):
         self.current_waypoint = self.waypoints[0]
         self.waypoint_index = 0
 
-    @state(first=True)
+    def on_enable(self) -> None:
+        self.chassis.reset_odometry(wpilib.geometry.Pose2d())
+        super().on_enable()
+
+    @state
     def shoot(self) -> None:
         """
         Shoot all balls that we currently have
         """
-        self.shootercontroller.engage()
-        self.shootercontroller.fire_command()
+        self.shooter_controller.engage()
+        self.shooter_controller.fire_command()
         if self.indexer.balls_loaded == 0:
             self.next_state("move")
 
-    @state
+    @state(first=True)
     def move(self, initial_call):
         """
         Follow the trajectory defined by our waypoints
@@ -56,31 +63,32 @@ class ShootMoveShootBase(AutonomousStateMachine):
                 # end the statemachine if we re-enter move
                 # with no new trajectory
                 self.done()
+                return
         pos = self.chassis.get_pose()
         if self.current_waypoint is None:
             self.chassis.drive(0, 0)
             self.next_state("shoot")
-        vx, _, vz = self.controller.calculate(pos, self.current_waypoint, 3, 0)
-        self.chassis.drive(vx, vz)
-        error = self.current_waypoint.relativeTo(pos)
-        if (
-            abs(error.translation) < self.pos_tolerance
-            and abs(error.rotation) < self.ang_tolerance
-        ):
+            return
+        speeds = self.controller.calculate(pos, self.current_waypoint, 1, 0)
+        self.chassis.drive(speeds.vx, speeds.omega)
+        if self.controller.atReference():
             self.current_waypoint = self.get_next_waypoint()
 
-    def get_next_waypoint(self) -> Optional(wpilib.geometry.Pose2d):
+    def get_next_waypoint(self) -> Optional[wpilib.geometry.Pose2d]:
         """
         Return the next waypoint in the que, returns none if there is none
         """
-        if len(self.waypoints) > self.waypoint_index:
+        if len(self.waypoints)-1 > self.waypoint_index:
             self.waypoint_index += 1
             return self.waypoints[self.waypoint_index]
         else:
             return None
 
 
-class Basic3Forward3(ShootMoveShootBase):
+class test(ShootMoveShootBase):
+    MODE_NAME = "Test"
+    DEFAULT = True
+
     def __init__(self) -> None:
-        self.waypoints = (to_pose(1, 0, 0), to_pose(2, 1, -math.pi / 4))
+        self.waypoints = [to_pose(2, 0.5, 0)]
         super().__init__()
