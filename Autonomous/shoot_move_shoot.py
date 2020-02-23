10550df5c49b53060b8fbadded03bca1583dc326
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2020-02-23 15:04:04 +1100

    Use ramsete properly

--- Autonomous/shoot_move_shoot.py
+++ Autonomous/shoot_move_shoot.py
@@ -1,4 +1,5 @@
 import math
+import time
 from typing import Optional
 
 from wpilib import controller
@@ -28,13 +29,15 @@ class ShootMoveShootBase(AutonomousStateMachine):
 
     def __init__(self) -> None:
         super().__init__()
-        self.controller = controller.RamseteController(1, 0.7)
+        self.controller = controller.RamseteController()
         tolerance = to_pose(0.1, 0.1, math.pi / 18)
         self.controller.setTolerance(tolerance)
         self.trajectory_config = trajectory.TrajectoryConfig(
             maxVelocity=1, maxAcceleration=1
         )
         self.gen = trajectory.TrajectoryGenerator()
+        self.trajectory_num = 0
+        self.trajectory_max = 1
 
     def setup(self):
         self.trajectory_config.setKinematics(self.chassis.kinematics)
@@ -43,40 +46,34 @@ class ShootMoveShootBase(AutonomousStateMachine):
         self.chassis.reset_odometry(geometry.Pose2d())
         super().on_enable()
 
-    @state
+    @state(first=True)
     def shoot(self) -> None:
         """
         Shoot all balls that we currently have
         """
         self.shooter_controller.engage()
-        self.shooter_controller.fire_command()
-        if self.indexer.balls_loaded == 0:
-            self.next_state("move")
+        self.shooter_controller.fire_input()
+        if self.indexer.balls_loaded() == 0:
+            if self.trajectory_num >= self.trajectory_max:
+                self.done()
+            else:
+                self.next_state("move")
 
-    @state(first=True)
-    def move(self, initial_call) -> None:
+    @state
+    def move(self, initial_call, state_tm) -> None:
         """
         Follow the trajectory defined by our waypoints
         """
-        pos = self.chassis.get_pose()
-        speeds = self.controller.calculate(pos, self.goal)
-        self.chassis.drive(speeds.vx, speeds.omega)
-        if self.controller.atReference():
-            self.current_waypoint_id += 1
-            if self.current_waypoint_id >= self.number_of_waypoints:
-                self.done()
-                return
-            self.goal = self.get_waypoint(self.current_waypoint_id)
-            print(self.goal.velocity)
-
-    def get_waypoint(self, waypoint_id: int) -> Optional[trajectory.Trajectory.State]:
-        """Get waypoint by a zero indexed id"""
-        if waypoint_id >= self.number_of_waypoints:
-            print("Attempted to track waypoint beyond number in trajectory.")
+        if state_tm > self.path.totalTime():
+            print(f"Calculated path time: {self.path.totalTime()}")
+            self.chassis.drive(0, 0)
+            self.next_state("shoot")
+            self.trajectory_num += 1
             return
-        time = self.time_per_waypoint * (waypoint_id + 1)
-        waypoint = self.path.sample(time)
-        return waypoint
+        pos = self.chassis.get_pose()
+        goal = self.path.sample(state_tm)
+        speeds = self.controller.calculate(pos, goal)
+        self.chassis.drive(speeds.vx, speeds.omega)
 
 
 class test(ShootMoveShootBase):
@@ -85,20 +82,16 @@ class test(ShootMoveShootBase):
 
     def __init__(self) -> None:
         super().__init__()
+
+    def setup(self):
+        super().setup()
         self.start_pose = to_pose(0, 0, 0)
-        self.end_pose = to_pose(4, 0, 0)
-        self.waypoints = [geometry.Translation2d(2, 0), geometry.Translation2d(3, 0)]
+        self.chassis.reset_odometry(self.start_pose)
+        self.end_pose = to_pose(2, 0, 0)
+        self.waypoints = [geometry.Translation2d(1, 0)]
         self.trajectory_config = trajectory.TrajectoryConfig(
             maxVelocity=1, maxAcceleration=1
         )
         self.path = self.gen.generateTrajectory(
             self.start_pose, self.waypoints, self.end_pose, self.trajectory_config
         )
-
-        self.number_of_waypoints = 5
-        self.total_time = self.path.totalTime()
-        self.time_per_waypoint = self.total_time / self.number_of_waypoints
-
-        self.current_waypoint_id = 0
-        self.goal = self.get_waypoint(self.current_waypoint_id)
-        self.current_waypoint_id += 1
