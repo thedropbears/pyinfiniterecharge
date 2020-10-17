ec4c32eb6868935333cfaa9a622397562539553f
Injector-Spenral <sam.richards@thedropbears.org.au>
2020-10-17 15:40:52 +1100

    Intake motors now spin when clearing.

--- components/indexer.py
+++ components/indexer.py
@@ -76,6 +76,7 @@ class Indexer:
             # Run everything backwards to try to clear a jam
             for motor in self.indexer_motors:
                 motor.set(-self.indexer_speed)
+            self.intake_main_motor.set(-self.intake_motor_speed)
             self.intake_left_motor.set(0)
             self.intake_right_motor.set(0)
             return
