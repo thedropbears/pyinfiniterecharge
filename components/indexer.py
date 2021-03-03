6d4e413db46fcded570f0c171791bf1ff9de931c
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2021-03-03 19:10:19 +1100

    Check if there is a ball in motion towards the feeder

--- components/indexer.py
+++ components/indexer.py
@@ -55,6 +55,7 @@ class Indexer:
         # stage of the indexer and the injector is longer than others
         # and the injector motors are slower
         self.transfer_to_injector = False
+        self.transfer_to_feeder = False
         self.left_shimmy = True
 
         self.shimmy_count = 0
@@ -93,6 +94,10 @@ class Indexer:
         elif feeder.isFwdLimitSwitchClosed():
             # Transferring
             self.transfer_to_injector = True
+        if feeder.isFwdLimitSwitchClosed():
+            self.transfer_to_feeder = False
+        elif self.indexer_motors[-2].isFwdLimitSwitchClosed():
+            self.transfer_to_feeder = True
 
         # Find the location of the closest ball to the intake,
         # counting from the intake (0).  Only run the motors from
@@ -178,7 +183,7 @@ class Indexer:
     def balls_loaded(self) -> int:
         balls = (
             sum(motor.isFwdLimitSwitchClosed() for motor in self.all_motors)
-            + self.transfer_to_injector
+            + self.transfer_to_injector + self.transfer_to_feeder
         )
         return balls
 
